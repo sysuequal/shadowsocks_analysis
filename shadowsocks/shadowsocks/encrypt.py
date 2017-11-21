101882aec2106feac05b64f97256390d867a7407
yongchao-long <260710094@qq.com>
2017-11-21 20:33:38 +0800

    Update encrypt.py

--- shadowsocks/shadowsocks/encrypt.py
+++ shadowsocks/shadowsocks/encrypt.py
@@ -25,7 +25,7 @@ import logging
 from shadowsocks import common
 from shadowsocks.crypto import rc4_md5, openssl, sodium, table
 
-#支持的加密算法
+# 支持的加密算法
 method_supported = {}
 method_supported.update(rc4_md5.ciphers)
 method_supported.update(openssl.ciphers)
@@ -36,10 +36,10 @@ method_supported.update(table.ciphers)
 def random_string(length):
     """
     随机生成n字节字符串
-    
-    :param length:随机字符串的长度
-    
-    :return:n字节字符串
+
+    :param length: 随机字符串的长度
+
+    :return: n字节字符串
     """
     return os.urandom(length)
 
@@ -50,10 +50,12 @@ cached_keys = {}
 def try_cipher(key, method=None):
     """
     尝试加密
-    
-    :param key:用于生成密钥的key
-    
-    :param method:加密算法
+
+    :param key: 用于生成密钥的key
+
+    :param method: 加密算法
+
+    :return: None
     """
     Encryptor(key, method)
 
@@ -61,14 +63,14 @@ def try_cipher(key, method=None):
 def EVP_BytesToKey(password, key_len, iv_len):
     """
     生成key和密钥初始化矢量iv
-    
-    :param password:用于生成key和iv
-    
-    :param key_len:key的规定长度
-    
-    :param iv-len:iv的规定长度
-    
-    :return:密钥key和密钥初始化向量iv
+
+    :param password: 用于生成key和iv
+
+    :param key_len: key的规定长度
+
+    :param iv_len: iv的规定长度
+
+    :return: 密钥key和密钥初始化向量iv
     """
     # equivalent to OpenSSL's EVP_BytesToKey() with count 1
     # so that we make the same key and iv as nodejs version
@@ -92,15 +94,15 @@ def EVP_BytesToKey(password, key_len, iv_len):
     cached_keys[cached_key] = (key, iv)
     return key, iv
 
-#加密器
+
 class Encryptor(object):
     def __init__(self, key, method):
         """
         初始化加密器
-        
-        :param key:密钥
-        
-        :param method:加密算法
+
+        :param key: 密钥
+
+        :param method: 加密算法
         """
         self.key = key
         self.method = method
@@ -120,10 +122,10 @@ class Encryptor(object):
     def get_method_info(self, method):
         """
         获取加密算法的信息
-        
-        :param method:加密算法
-        
-        :return:加密算法信息m,m[0]key的长度,m[1]iv的长度,m[2]加密算法
+
+        :param method: 加密算法
+
+        :return: 加密算法信息m,m[0]key的长度,m[1]iv的长度,m[2]加密算法
         """
         method = method.lower()
         m = method_supported.get(method)
@@ -132,25 +134,25 @@ class Encryptor(object):
     def iv_len(self):
         """
         获得iv的长度
-        
-        :return:密钥初始化向量iv长度
+
+        :return: 密钥初始化向量iv长度
         """
         return len(self.cipher_iv)
 
     def get_cipher(self, password, method, op, iv):
-         """
-         获得加密算法加密器,m[0]key的长度,m[1]iv的长度,m[2]加密算法
-         
-         :param password:用于生成key和iv
-         
-         :param method:加密算法
-         
-         :param op:操作标识，1是加密，0是解密
-         
-         :param iv:key初始化矢量
-         
-         :return:加密器
-         """
+        """
+        获得加密算法加密器,m[0]key的长度,m[1]iv的长度,m[2]加密算法
+
+        :param password: 用于生成key和iv
+
+        :param method: 加密算法
+
+        :param op: 操作标识，1是加密，0是解密
+
+        :param iv: key初始化矢量
+
+        :return: 加密器
+        """
         password = common.to_bytes(password)
         m = self._method_info
         if m[0] > 0:
@@ -168,10 +170,10 @@ class Encryptor(object):
     def encrypt(self, buf):
         """
         加密
-        
-        :param buf:需要加密数据
-        
-        :return:密文
+
+        :param buf: 需要加密数据
+
+        :return: 密文
         """
         if len(buf) == 0:
             return buf
@@ -184,10 +186,10 @@ class Encryptor(object):
     def decrypt(self, buf):
         """
         解密
-        
-        :param buf:需要解密数据
-        
-        :return:正文
+
+        :param buf: 需要解密数据
+
+        :return: 正文
         """
         if len(buf) == 0:
             return buf
@@ -204,17 +206,17 @@ class Encryptor(object):
 
 def encrypt_all(password, method, op, data):
     """
-    加密算法
-    
-    :param method:加密算法
-    
-    :param password:用于生成key
-    
-    :param op:操作标识，op=1加密，op=0解密、
-    
-    :param data:加密/解密数据
-    
-    :return:密文
+    加密算法加密
+
+    :param password: 加密算法
+
+    :param method: 用于生成key
+
+    :param op: 操作标识，op=1加密，op=0解密
+
+    :param data: 加密/解密数据
+
+    :return: 密文
     """
     result = []
     method = method.lower()
@@ -233,7 +235,7 @@ def encrypt_all(password, method, op, data):
     result.append(cipher.update(data))
     return b''.join(result)
 
-#所有加密算法
+
 CIPHERS_TO_TEST = [
     'aes-128-cfb',
     'aes-256-cfb',
@@ -246,7 +248,9 @@ CIPHERS_TO_TEST = [
 
 def test_encryptor():
     """
-    测试所有加密器加密和解密
+    测试所有加密算法加密器加密和解密
+
+    :return: None
     """
     from os import urandom
     plain = urandom(10240)
@@ -262,6 +266,8 @@ def test_encryptor():
 def test_encrypt_all():
     """
     测试所有加密算法的加密和解密
+
+    :return: None
     """
     from os import urandom
     plain = urandom(10240)
